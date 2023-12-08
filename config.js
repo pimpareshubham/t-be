3ee38d14ef1591934ea829bccb25a25dc594e81f
pimpareshubham <127371609+pimpareshubham@users.noreply.github.com>
2023-12-08 16:27:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGODB_URL: "mongodb://127.0.0.1:27017/Twitter_clone",
+    MONGODB_URL: "mongodb+srv://shubhampimpare111:PQ6VjeTRQGWZZuFk@cluster0.0obowjk.mongodb.net/?retryWrites=true&w=majority",
     JWT_SECRET: "hafsfffgg72846285jjjkh54646467hh"
 };
